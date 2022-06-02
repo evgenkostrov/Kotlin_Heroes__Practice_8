7e632f81afed3831221c9423d707e4e4fb92fe95
evgenkostrov <evgenkostrov@gmail.com>
2022-06-02 23:56:22 +0300

    accomplish C

--- Problems/C. Стрельба/src/Main.kt
+++ Problems/C. Стрельба/src/Main.kt
@@ -5,5 +5,17 @@
  */
 
 fun main() {
-    // Write your solution here
+  val n = readInt()
+    val a = readInts()
+    val b = a.withIndex().sortedByDescending { it.value }
+    var res = 0
+    for(i in 0 until n){
+        res+=b[i].value*i + 1
+    }
+    println(res)
+    println(b.joinToString (" "){ "${it.index +1 }"})
 }
+
+
+private fun readInt() = readLine()!!.toInt()
+private fun readInts() = readLine()!!.split(" ").map { it.toInt() }
