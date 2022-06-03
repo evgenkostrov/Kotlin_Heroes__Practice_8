4dbfac5705e2eb6021fdaa77f3ad245c7229ce37
evgenkostrov <evgenkostrov@gmail.com>
2022-06-03 23:50:30 +0300

    accomplish DE

--- Problems/D. Достижимые числа/src/Main.kt
+++ Problems/D. Достижимые числа/src/Main.kt
@@ -5,5 +5,17 @@
  */
 
 fun main() {
-    // Write your solution here
+var n =readInt()
+    val s = HashSet<Int>()
+    while (!s.contains(n)){ //while(s.add(n))
+        s.add(n)
+        n +=1
+        while (n%10 == 0 ){
+            n/=10
+        }
+    }
+    println(s.size)
 }
+
+private fun readInt() = readLine()!!.toInt()
+private fun readInts() = readLine()!!.split(" ").map { it.toInt() }
--- Problems/E. Удаление нулей/src/Main.kt
+++ Problems/E. Удаление нулей/src/Main.kt
@@ -5,5 +5,18 @@
  */
 
 fun main() {
-    // Write your solution here
+    repeat(readInt()) {
+        var s = readLine()!!
+        val i = s.indexOf('1')
+        if (i == -1) {
+            println(0)
+        } else {
+            val j = s.lastIndexOf('1')
+            val ans = s.substring(i until j).count { it == '0' }
+            println(ans)
+        }
+    }
 }
+
+private fun readInt() = readLine()!!.toInt()
+private fun readInts() = readLine()!!.split(" ").map { it.toInt() }
